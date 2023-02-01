c8d2c971f54112d6b35307d1dea54d5ebad28357
Nader Elhaj <naderelhaj3@gmail.com>
2023-02-01 23:03:04 +0100

    Docker implemented

--- client/script.js
+++ client/script.js
@@ -1,90 +1,103 @@
-import bot from "./assets/bot.svg";
-import user from "./assets/user.svg";
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
 
-const form = document.querySelector("form");
-const chatContainer = document.querySelector("#chat_container");
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
-let loadInterval;
+let loadInterval
 
-//  loading messages
-function loader(element) {
-  element.textContent = "";
-  loadInterval = setInterval(() => {
-    element.textContent += ".";
-    if (element.textContent === "....") {
-      element.textContent = "";
-    }
-  }, 300);
+function showLoading(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        element.textContent += '.'
+        if (element.textContent === '....') {
+            element.textContent = ''
+        }
+    }, 300)
 }
-
-// typing message letter by letter
 
 function typeText(element, text) {
-  let index = 0;
-  let interval = setInterval(() => {
-    if (index < text.length) {
-      element.innerHtml += text.charAt(index);
-      index++;
-    } else {
-      clearInterval(interval);
-    }
-  }, 20);
-}
+    let index = 0
 
-// generating unique id for each message using the current timestamp
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
 
 function generateUniqueId() {
-  const timestamp = Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
-  return `id-${timestamp}-${hexadecimalString}`;
-}
+    const timestamp = Date.now()
+    const randomNumber = Math.random()
+    const hexadecimalString = randomNumber.toString(16)
 
-//Ai or User recognition
+    return `id-${timestamp}-${hexadecimalString}`
+}
 
 function chatStripe(isAi, value, uniqueId) {
-  return `
-      <div class="wrapper ${isAi && "ai"}">
-          <div class="chat">
-              <div class="profile">
-                  <img 
-                    src=${isAi ? bot : user} 
-                    alt="${isAi ? "bot" : "user"}" 
-                  />
-              </div>
-              <div class="message" id=${uniqueId}>${value}</div>
-          </div>
-      </div>
-  `;
+    return (
+        `
+        <div class="wrapper ${isAi ? 'ai' : ''}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
 }
 
-const handleSumbit = async (e) => {
-  e.preventDefault();
+async function handleSubmit(e) {
+    e.preventDefault()
 
-  const data = new FormData(form);
+    const data = new FormData(form)
 
-  //users ChatStripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+    form.reset()
 
-  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
-  form.reset();
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, ' ', uniqueId)
+    chatContainer.scrollTop = chatContainer.scrollHeight
 
-  //bots ChatStripe
-  const uniqueId = generateUniqueId(); 
-  chatContainer.innerHTML += chatStripe(true, " ",uniqueId);
+    const messageDiv = document.getElementById(uniqueId)
+    showLoading(messageDiv)
 
-  chatContainer.scrollTop = chatContainer.scrollHeight ; 
+    const response = await fetch('https://codex-im0y.onrender.com/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': "Bearer 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"
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt')
+      })
+    })
 
-  const messageDiv = document.getElementById(uniqueId) ; 
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = ''
 
-  loader(messageDiv) ; 
+    if (response.ok) {
+      const data = await response.json()
+      const parsedData = data.bot.trim()
+      typeText(messageDiv,parsedData )
+    } else {
+      const err = await response.text()
 
-
-};
-
-form.addEventListener('submit',handleSumbit) ; 
-form.addEventListener('keyup',(e)=>{
-  if(e.keyCode === 13){
-    handleSumbit(e)
-  }
-}) ; 
-
+      messageDiv.innerHTML = 'Something went wrong'
+      console.log(err)
+    }}
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
